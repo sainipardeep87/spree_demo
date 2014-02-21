42a6faedd1549eb965283a98f35611c47fa44f87
Pardeep <pardeep@impinge47-G31M-S2L.(none)>
2014-02-21 14:20:17 +0530

    basic skellon created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecommerce::Application.config.secret_key_base = 'dc55fd59a69e235e98821910eb1d72a0735fb8418ca044a931bd7d4d8287e556658e58dd316f522538b0d1fb71e56c076d9bf3e7151ae71614668d90b1f25432'
